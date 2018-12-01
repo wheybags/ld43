710a995fc37e969b7ad77e807ce8be0697842673
Jiří Šebele <sindriava@gmail.com>
2018-12-01 13:36:50 +0100

    Add a simple movable player

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    public float speed = 4.0f;
+	
+	void Update () {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        var force = new Vector2(horizontal, vertical);
+
+        GetComponent<Rigidbody2D>().AddForce(force * speed);
+	}
+}
