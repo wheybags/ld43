95e34e82255036dce20e4e151be5b3162ee44ac7
Jiří Šebele <sindriava@gmail.com>
2018-12-01 18:19:08 +0100

    Add a health bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+    public int current = 3;
+    public int maximum = 3;
+
+    public Image[] images;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+	
+	void Update () {
+        for (int i = 0; i < images.Length; i++) {
+            images[i].enabled = i < maximum;
+            images[i].sprite = i < current ? fullHeart : emptyHeart;
+        }
+    }
+
+    public void Add() {
+        if (current < maximum) {
+            current += 1;
+        }
+    }
+}
