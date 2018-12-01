cb0f76b16e6cce43904b99c74ebb2ea82b2988dc
Tom Mason <wheybags@wheybags.com>
2018-12-01 16:11:53 +0100

    Weapons prototype

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class Mover : MonoBehaviour {
     public float speed = 4.0f;
 
+    public GameObject weapon = null;
+    public GameObject defaultWeapon;
+
     private new Rigidbody2D rigidbody;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-
         rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        SetWeapon(defaultWeapon);
+    }
+
+    private void SetWeapon(GameObject newWeapon)
+    {
+        weapon = Instantiate(newWeapon);
+        weapon.transform.parent = transform;
+        weapon.transform.position = new Vector2(0, weapon.GetComponent<BoxCollider2D>().bounds.max.y);
     }
 
     void FixedUpdate()
