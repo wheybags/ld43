2828f7c098c7b13dd9ec8b36656e2c2b12fb9948
Jiří Šebele <sindriava@gmail.com>
2018-12-01 16:46:22 +0100

    Add food, health and teeth handicap

--- Assets/Scripts/Handicap.cs
+++ Assets/Scripts/Handicap.cs
@@ -6,4 +6,5 @@ public class Handicap : MonoBehaviour {
     public bool leftEye = true;
     public bool rightEye = true;
     public bool legs = true;
+    public bool teeth = true;
 }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -11,12 +11,19 @@ public class Mover : MonoBehaviour {
     private new Rigidbody2D rigidbody;
     private Handicap handicap;
 
+    private int maxHealth = 100;
+    private int health;
+
+    private float timeOut = 0.0f;
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
 
         handicap = GetComponent<Handicap>();
+
+        health = maxHealth;
 
         SetWeapon(defaultWeapon);
     }
@@ -30,15 +37,24 @@ public class Mover : MonoBehaviour {
 
     void FixedUpdate()
     {
+        if (timeOut > 0.0f) {
+            timeOut -= Time.deltaTime;
+            if (timeOut < 0.0f) {
+                timeOut = 0.0f;
+            }
+        }
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         float actualSpeed = handicap.legs ? speed : speed / 12.0f;
 
-        rigidbody.velocity = new Vector2(
-            Mathf.Lerp(0, Input.GetAxis("Horizontal") * actualSpeed, 0.8f),
-            Mathf.Lerp(0, Input.GetAxis("Vertical")   * actualSpeed, 0.8f)
-        );
+        if (timeOut == 0.0f) {
+            rigidbody.velocity = new Vector2(
+                Mathf.Lerp(0, Input.GetAxis("Horizontal") * actualSpeed, 0.8f),
+                Mathf.Lerp(0, Input.GetAxis("Vertical") * actualSpeed, 0.8f)
+            );
+        }
 
         Vector2 position = transform.position;
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -48,5 +64,18 @@ public class Mover : MonoBehaviour {
         weapon.transform.rotation = Quaternion.Euler(0, 0, angle);
 
         Debug.DrawLine(position, position + direction * 2, Color.red, 0, false);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Food") {
+            col.gameObject.SetActive(false);
+            health += 10;
+            if (health > maxHealth) {
+                health = maxHealth;
+            }
+            if (!handicap.teeth) {
+                timeOut = 3.0f;
+            }
+        }
     }
 }
