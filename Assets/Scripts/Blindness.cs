f31b7506dc24507b50e2f4bc2da1f30ba33e7c71
Jiří Šebele <sindriava@gmail.com>
2018-12-01 16:28:14 +0100

    Extract blindness to a separate script

---
+++ Assets/Scripts/Blindness.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blindness : MonoBehaviour {
+    public UnityEngine.UI.Image blindnessL;
+    public UnityEngine.UI.Image blindnessR;
+
+    private Vector3 blindnessLDelta;
+    private Vector3 blindnessRDelta;
+
+    private Handicap handicap;
+
+    void Start() {
+        handicap = GetComponent<Handicap>();
+
+        blindnessLDelta = blindnessL.rectTransform.position - transform.position;
+        blindnessRDelta = blindnessR.rectTransform.position - transform.position;
+    }
+
+    void Update() {
+        blindnessL.rectTransform.position = transform.position + blindnessLDelta;
+        blindnessR.rectTransform.position = transform.position + blindnessRDelta;
+
+        blindnessL.enabled = !handicap.leftEye;
+        blindnessR.enabled = !handicap.rightEye;
+    }
+}
--- Assets/Scripts/Handicap.cs
+++ Assets/Scripts/Handicap.cs
@@ -3,26 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Handicap : MonoBehaviour {
-    public UnityEngine.UI.Image blindnessL;
-    public UnityEngine.UI.Image blindnessR;
-
     public bool leftEye = true;
     public bool rightEye = true;
     public bool legs = true;
-
-    private Vector3 blindnessLDelta;
-    private Vector3 blindnessRDelta;
-
-    void Start() {
-        blindnessLDelta = blindnessL.rectTransform.position - transform.position;
-        blindnessRDelta = blindnessR.rectTransform.position - transform.position;
-    }
-
-    void Update () {
-        blindnessL.rectTransform.position = transform.position + blindnessLDelta;
-        blindnessR.rectTransform.position = transform.position + blindnessRDelta;
-
-        blindnessL.enabled = !leftEye;
-        blindnessR.enabled = !rightEye;
-    }
 }
