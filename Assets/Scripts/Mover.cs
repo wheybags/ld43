6efc536d13d6090058911ba497a637ce3b183c8a
Jiří Šebele <sindriava@gmail.com>
2018-12-01 14:06:20 +0100

    Improve player movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Mover : MonoBehaviour {
     public float speed = 4.0f;
 
-    void Update () {
-        var vertical = Input.GetAxis("Vertical");
-        var horizontal = Input.GetAxis("Horizontal");
+    private new Rigidbody2D rigidbody;
 
-        var force = new Vector2(horizontal, vertical);
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
 
-        GetComponent<Rigidbody2D>().AddForce(force * speed);
+        rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(
+            Mathf.Lerp(0, Input.GetAxis("Horizontal") * speed, 0.8f),
+            Mathf.Lerp(0, Input.GetAxis("Vertical")   * speed, 0.8f)
+        );
     }
 }
