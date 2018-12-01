321d954de0661fcdf524170d64653eb4ea6e59ff
Tom Mason <wheybags@wheybags.com>
2018-12-01 13:44:23 +0100

    replace tabs with spaces

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour {
     public float speed = 4.0f;
-	
-	void Update () {
+
+    void Update () {
         var vertical = Input.GetAxis("Vertical");
         var horizontal = Input.GetAxis("Horizontal");
 
         var force = new Vector2(horizontal, vertical);
 
         GetComponent<Rigidbody2D>().AddForce(force * speed);
-	}
+    }
 }
