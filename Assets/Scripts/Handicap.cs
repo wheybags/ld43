dc4a5d6eadc74ebaf9024b245b2cae62bb4aca91
Jiří Šebele <sindriava@gmail.com>
2018-12-01 15:14:48 +0100

    Implement Handicap on the player
    # Conflicts:
    #	Assets/_Scenes/Room1.unity

---
+++ Assets/Scripts/Handicap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handicap : MonoBehaviour {
+    public UnityEngine.UI.Image blindnessL;
+    public UnityEngine.UI.Image blindnessR;
+
+    public bool leftEye = true;
+    public bool rightEye = true;
+
+    private Vector3 blindnessLDelta;
+    private Vector3 blindnessRDelta;
+
+    void Start() {
+        blindnessLDelta = blindnessL.rectTransform.position - transform.position;
+        blindnessRDelta = blindnessR.rectTransform.position - transform.position;
+    }
+
+    void Update () {
+        blindnessL.rectTransform.position = transform.position + blindnessLDelta;
+        blindnessR.rectTransform.position = transform.position + blindnessLDelta;
+
+        blindnessL.enabled = !leftEye;
+        blindnessR.enabled = !rightEye;
+	}
+}
