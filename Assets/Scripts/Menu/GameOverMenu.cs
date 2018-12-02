a5f52078684dd330f9847132fc34955960a5ddcc
Martin Hanzík <martin@hanzik.com>
2018-12-02 17:50:21 +0100

    Better game over screen

---
+++ Assets/Scripts/Menu/GameOverMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverMenu : MonoBehaviour
+{
+	void Start () {
+		transform.Find("Play").GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Room1"));
+		transform.Find("Main menu").GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Menu"));
+	}	
+}
