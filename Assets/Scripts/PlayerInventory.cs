b2516dc89dda3944ba5760edbd29973ae311fb1f
Sanqui <gsanky@gmail.com>
2018-12-02 13:34:43 +0100

    Show when the player has a key in UI

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -5,4 +5,10 @@ using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour {
     public List<int> KeyIds = new List<int>();
+    public GameObject KeySprite;
+    
+    
+	void Update () {
+        KeySprite.SetActive(KeyIds.Count >= 1);
+    }
 }
