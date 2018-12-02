55dc69a7f4cf91cae46d5fdb9abe5de371eed4ed
Martin Hanzík <martin@hanzik.com>
2018-12-02 17:14:53 +0100

    Add menu

---
+++ Assets/Scripts/Menu/CreditsMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsMenu : MonoBehaviour
+{
+	void Start () {
+		transform.Find("Back").GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Menu"));
+	}	
+}
---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	void Start () {
+		transform.Find("Play").GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Room1"));
+		transform.Find("Credits").GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Credits"));
+		transform.Find("Quit").GetComponent<Button>().onClick.AddListener(() => Application.Quit());
+	}	
+}
