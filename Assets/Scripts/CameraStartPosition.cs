9b636d154b2deb0b3b14deb3c0c1e2ccb5761fb3
Martin Hanzík <martin@hanzik.com>
2018-12-02 15:02:29 +0100

    Move camera to room at startup

---
+++ Assets/Scripts/CameraStartPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStartPosition : MonoBehaviour
+{
+
+	public Transform FirstRoom;
+	
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(FirstRoom.position.x, FirstRoom.position.y, -10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
