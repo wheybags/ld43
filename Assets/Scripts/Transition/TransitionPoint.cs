a3f9cba8d037aa01a4571434b7c79f23567289b0
Martin Hanzík <martin@hanzik.com>
2018-12-02 13:01:39 +0100

    Rooms with camera

--- Assets/Scripts/Transition/TransitionPoint.cs
+++ Assets/Scripts/Transition/TransitionPoint.cs
@@ -6,13 +6,22 @@ public class TransitionPoint : MonoBehaviour
 {
     [Tooltip("The destination in this scene that the transitioning gameobject will be teleported.")]
     public Transform Destination;
+    public Transform CameraDestination;
 
+    private GameObject _camera;
+
+    void Start()
+    {
+        _camera = GameObject.Find("Main Camera");
+    }
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.transform.position = Destination.transform.position;
             other.gameObject.transform.rotation = Destination.transform.rotation;
+            _camera.transform.position = new Vector3(CameraDestination.position.x, CameraDestination.position.y, -10);
         }
     }
 }
