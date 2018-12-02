49eb44bf0c22eb757bed81b1e7568ad9b526270b
Sanqui <gsanky@gmail.com>
2018-12-02 13:04:52 +0100

    Player can now collect a key

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+    public int KeyID;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// shine
+	}
+	
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("test");
+        if (collision.gameObject.tag == "Player") {
+            collision.gameObject.GetComponent<PlayerInventory>().KeyIds.Add(KeyID);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventory : MonoBehaviour {
+    public List<int> KeyIds = new List<int>();
+}
