b8336b2000b68beb2178719215b9241daf7d5681
Martin Hanzík <martin@hanzik.com>
2018-12-02 12:31:28 +0100

    Allow in-scene teleport

--- Assets/Scripts/SceneTransition/TransitionPoint.cs
+++ Assets/Scripts/SceneTransition/TransitionPoint.cs
@@ -4,17 +4,36 @@ using SceneTransition;
 [RequireComponent(typeof(Collider2D))]
 public class TransitionPoint : MonoBehaviour
 {
-    [SceneName]
-    public string NewSceneName;
+    public enum TransitionType
+    {
+        DifferentScene,
+        SameScene
+    }
+
+    [SceneName] public string NewSceneName;
+
+    [Tooltip("Whether the transition will be within this scene, to a different zone or a non-gameplay scene.")]
+    public TransitionType Type;
 
     [Tooltip("The tag of the SceneTransitionDestination script in the scene being transitioned to.")]
     public DestinationTag TransitionDestinationTag;
+
+    [Tooltip("The destination in this scene that the transitioning gameobject will be teleported.")]
+    public TransitionDestination Destination;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneController.TransitionToScene(this);
+            if (Type == TransitionType.SameScene)
+            {
+                other.gameObject.transform.position = Destination.transform.position;
+                other.gameObject.transform.rotation = Destination.transform.rotation;
+            }
+            else
+            {
+                SceneController.TransitionToScene(this);
+            }
         }
     }
 }
