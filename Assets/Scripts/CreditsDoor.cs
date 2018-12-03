cf1d8ac96599b69c72b88b7407652cc534fe30b6
Sanqui <gsanky@gmail.com>
2018-12-03 11:47:36 +0100

    Make last door lead to credits

---
+++ Assets/Scripts/CreditsDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditsDoor : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            SceneManager.LoadScene("Credits");
+        }
+    }
+}
